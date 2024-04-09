af109d859b3d97bb5eedee5170b6762893bf8222
Emyoussef <159749616+Emyoussef@users.noreply.github.com>
2024-04-09 21:18:02 +0000

    Update Testing_Software.sh
    Out final testing bash script that runs the program with a selected set of inputs for the program and sends the output to a file.

--- Testing_Software.sh
+++ Testing_Software.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# Remove existing Output.txt if it exists
-rm Output.txt
-
 # Provide input to the Python script and capture the output
 echo "Running Python script with input..."
 python3 test_Graphhopper.py << testFile > Output.txt
@@ -10,6 +6,13 @@ car  # Input
 washinton,DC #Input
 Baltimore,MD #Input
 miles #input
-q #input
+bike #input
+hawaii #Input
+California #Input
+kilometers #Input
+foot #Input
+Maine #Input
+Brazil #Input
+q #Input
 testFile
 
